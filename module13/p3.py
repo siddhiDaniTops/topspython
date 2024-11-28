f3ce135c2c59ef5567502dbeecfd1c931f9bc72b
siddhidani90 <danisiddhi6@gmail.com>
2024-11-28 22:35:48 +0530

    initial changes

---
+++ module13/p3.py
@@ -0,0 +1,8 @@
+#•	Write a Python program to demonstrate the creation of variables and different data types. 
+# example 1
+  # comment 
+ # var_name = value'''
+#example 2
+num=10
+x=int(input("enter number :"))
+print(type(x))
---
+++ module13/p4.py
@@ -0,0 +1,7 @@
+x=int(input("enter the number:"))
+y=int(input("enter the number :"))
+
+if(x>y):
+    print("x grater than y:")
+else:
+    print("y grater than x ")    
